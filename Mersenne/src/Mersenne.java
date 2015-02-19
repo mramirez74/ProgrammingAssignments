fb7f4a7b8b3e5689e26897fb008f366d47ffc9b8
mramirez74 <mramirez74@mail.stmarytx.edu>
2015-02-19 11:47:21 -0600

    Finding the Mersenne numbers of the numbers smaller than the user input

---
+++ Mersenne/src/Mersenne.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Mersenne {
+
+	public static void main(String[] args) {
+
+		System.out
+				.println("Enter a number. The system will find the Mersenne numbers smaller than this number.");
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+
+		for (int i = 0; i < number; i++) {
+
+			double mersenne = (Math.pow(2, i) - 1);
+			int Mersenne = (int) mersenne;
+			System.out.print(Mersenne + " ");
+			Mersenne++;
+
+		}
+
+		System.out.print("\nThe number of Mersenne numbers are " + number);
+	}
+
+}
