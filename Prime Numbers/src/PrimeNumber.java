82b1a416b752cca08a3e2f408bc235c103d2751b
mramirez74 <mramirez74@mail.stmarytx.edu>
2015-02-19 11:50:26 -0600

    Finding the Prime numbers less than the number of the user input

---
+++ Prime Numbers/src/PrimeNumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+
+		int prime = 0;
+		System.out
+				.println("Enter a number. The system will find the prime numbers smaller than this number.");
+
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+
+		System.out.println("These are the prime numbers smaller than " + number
+				+ ":\n");
+
+		if (number <= 1) {
+			System.out.println("There are no prime numbers less than 1!");
+		}
+
+		for (int i = 2; i < number; i++) {
+
+			for (int j = 2; j <= i; j++) {
+
+				if (j == i) {
+					System.out.print(i + " ");
+					prime++;
+				}
+				if (i % j == 0) {
+					break;
+				}
+
+			}
+		}
+
+		System.out.println("\nThere are a total of " + prime
+				+ " prime numbers.");
+	}
+
+}
